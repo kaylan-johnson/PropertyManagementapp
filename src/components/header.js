68f82ecb511e3bf362abe2b47152eba1e7999b0e
kaylan-johnson <drkaylan@gmail.com>
2020-06-15 10:21:48 -0600

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class Header extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Header</h1>
       </div>
     );
   }
 }
+export default Header;
