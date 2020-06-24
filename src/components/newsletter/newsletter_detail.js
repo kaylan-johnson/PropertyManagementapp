21acf88172a417f74034621e58dabf5c4c18a015
kaylan-johnson <drkaylan@gmail.com>
2020-06-24 08:22:27 -0600

    rendered fetched item on the screen

--- src/components/newsletter/newsletter_detail.js
+++ src/components/newsletter/newsletter_detail.js
@@ -6,10 +6,36 @@ class NewsletterDetail extends Component {
     componentDidMount() {
         this.props.fetchNewsletterById(this.props.match.params._id)
     }
+
+    renderFetchedItem() {
+        if(this.props.fetchedItem) {
+            return (
+                <div>
+                    <div>
+                        <img src={this.props.fetchedItem.imageUrl}/>
+                            <h2>{this.props.fetchedItem.title}</h2>
+                    </div>
+                        <p>
+                            {this.props.fetchedItem.body}
+
+                        </p>
+                </div>
+            )
+        }
+    }
     render() {
         return (
-        <div>
-            newsletter detail
+            <div className="row">
+            <div className="col-md-3 row">
+                <div style={{width: '200px', height:'200px', backgroundColor: 'skyblue'}}>
+                    02 Jan 18
+                </div>
+
+
+            </div>
+            <div className="col-md-9">
+            {this.renderFetchedItem()}
+            </div>
         </div>
         )
     }
