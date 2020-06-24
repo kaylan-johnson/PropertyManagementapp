335c351112a976d018080fb68c58b3a034001faf
kaylan-johnson <drkaylan@gmail.com>
2020-06-24 08:54:57 -0600

    implemented route for edit

---
+++ src/components/newsletter/newsletter_edit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditNewsletter extends Component {
+    render() {
+        return (
+            <form>
+            Form for edit
+            </form>
+        )
+    }
+}
+
+export default EditNewsletter;
