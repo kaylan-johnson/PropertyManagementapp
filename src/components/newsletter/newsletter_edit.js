10ed91299fd19d79199fff0198a01ffefd24d77a
kaylan-johnson <drkaylan@gmail.com>
2020-06-24 11:32:10 -0600

    set up save newsletter day action and promise using redux thunk

--- src/components/newsletter/newsletter_edit.js
+++ src/components/newsletter/newsletter_edit.js
@@ -23,6 +23,7 @@ class EditNewsletter extends Component {
     
         handleFormSubmit({title, body}) {
             console.log("trying to handle submit", title, body)
+            this.props.saveNewsletterEdit({title, body},this.props.params._id)
         }
     
         render() {
