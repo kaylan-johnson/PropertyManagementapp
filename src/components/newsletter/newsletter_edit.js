43903129641a3c444f4c6ef65aa3db7ffc710726
kaylan-johnson <drkaylan@gmail.com>
2020-06-24 09:16:55 -0600

    implemented redux-form into newsletter

--- src/components/newsletter/newsletter_edit.js
+++ src/components/newsletter/newsletter_edit.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 class EditNewsletter extends Component {
-    render() {
-        return (
-            <form>
-            Form for edit
-            </form>
-        )
-    }
+        renderInput(field) {
+            return <input className="form-control" {...field.input} type="field.type"/>
+        }
+    
+        handleFormSubmit({email, password}) {
+        }
+    
+        render() {
+            const { handleSubmit } = this.props;
+
+           return (
+           <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+               <label htmlFor="title">Title</label>
+               <Field name="title" component={this.renderInput} type="text"/>
+               <label htmlFor="body">Body</label>
+               <Field name="body" component={this.renderInput} type="text"/>
+               <Link to="/newsletter"><div>Cancel</div></Link>
+               <button action="submit" className="btn btn-primary">Save</button>
+    
+           </form>
+           )
+        }
 }
 
+EditNewsletter = reduxForm({form: "editNewsletter"})(EditNewsletter)
 export default EditNewsletter;
