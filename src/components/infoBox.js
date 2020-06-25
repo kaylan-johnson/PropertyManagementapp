fa35e9d49d587cfd24e2023514be6c0a446851f2
kaylan-johnson <drkaylan@gmail.com>
2020-06-25 08:33:49 -0600

    developed support request reducer and our a tag to click it

--- src/components/infoBox.js
+++ src/components/infoBox.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class InfoBox extends Component {
 
     handleSelected() {
-        
+        this.props.selectRequestType(this.props.type)
     }
 
 
     render() {
         return (
             <div className="info-box">
+            <a onClick={() => this.handleSelected()}>
                 the type of infobox is {this.props.type}
+                </a>
             </div>
         )
     }
 }
 
-export default InfoBox;
+export default connect(null, actions)(InfoBox);
