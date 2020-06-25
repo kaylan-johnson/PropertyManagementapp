23dc0ca8092bd11a8298368609a8411f47a6f722
kaylan-johnson <drkaylan@gmail.com>
2020-06-25 07:55:02 -0600

    added support request route and component

---
+++ src/components/supportRequest/supportRequest.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class SupportRequest extends Component {
+    render() {
+        return (
+            <div>
+                support request component
+               <div>
+                   <div>add request</div>
+                   <div className="row">
+                        <div className="col-md-3">
+                             3 infobox components
+                        </div>
+                        <div className="col-md-9">
+                            support request content component-contains ul
+                        </div>
+                   </div>
+               </div>
+            </div>
+        )
+    }
+}
+
+export default SupportRequest;
