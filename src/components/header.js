9014a763211a084c02c5e1c7746b00fcdd17a16e
kaylan-johnson <drkaylan@gmail.com>
2020-06-25 17:27:18 -0600

    added second way to render head HOC and fixed html nesting

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className="main-header">
-        <h1>Welcome to the HOA Manager!</h1>
-        <p>Please log in to continue</p>
-        {this.props.children}
+      <div>
+        <div className="main-header">
+          <h1>Welcome to the HOA Manager!</h1>
+          <p>Please log in to continue</p>
+          {this.props.children}
+        </div>
       </div>
     );
   }
